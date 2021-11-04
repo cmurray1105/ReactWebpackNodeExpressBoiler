8fb9e11237099bb6c1342b8893a6f351b6161684
cmurray1105 <--unset>
2021-11-04 12:49:00 -0500

    commit

--- Client/Source/index.js
+++ Client/Source/index.js
@@ -3,4 +3,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Appgit comfig --global';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
